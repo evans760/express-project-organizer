58703ea60f810254c919fb13d7a54f0402061474
Thomas <evans_760@yahoo.com>
2016-06-25 13:06:20 -0700

    New Revision.

--- dbTest.js
+++ dbTest.js
@@ -1,45 +1,21 @@
 var db = require('./models');
 
-db.project.find({
-  where: { id: 2 },
-  include: [db.category]
-}).then(function(project) {
-  // console.log(project.categories);
-
-  project.createCategory({ name: 'awesome'}).then(function(category) {
-    console.log(category.get());
-    });
-  });
-
-
-
-
-
-
-
-
-
-// var db = require('./models');
-
-// db.project.find({
-//   where: { id: 1 },
-//   include: [db.category]
-// }).then(function(project) {
-//   // by using eager loading, the project model should have a categories key
-//   console.log(project.categories);
-
-//   // a createCategory function should be available to this model
-//   project.createCategory({ name: 'node' }).then(function(category) {
-//     console.log(category.get());
-//   });
-// });
-
-
-
-// var db = require('./models');
-
-// db.project.create({
-//   name: 'turtle'
+// db.category.create({
+//   name: 'node'
 // }).then(function(category) {
 //   console.log(category.get());
 // });
+
+db.project.find({
+  where: { id: 1 },
+  include: [db.category]
+}).then(function(project) {
+  // by using eager loading, the project model should have a categories key
+  console.log(project.categories);
+
+  // a createCategory function should be available to this model
+  // project.createCategory({ name: 'node' }).then(function(category) {
+  //   console.log(category.get());
+  // });
+});
+
--- index.js
+++ index.js
@@ -4,12 +4,14 @@ var ejsLayouts = require('express-ejs-layouts');
 var db = require('./models');
 var app = express();
 
+
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(require('morgan')('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(ejsLayouts);
 
-app.get('/', function(req, res) { //route to homepage
+app.get('/', function(req, res) {
   db.project.findAll()
   .then(function(projects) {
     res.render('main/index', { projects: projects });
@@ -18,9 +20,10 @@ app.get('/', function(req, res) { //route to homepage
     res.status(400).render('main/404');
   });
 });
-//
+
 app.use('/projects', require('./controllers/projects'));
 
 var server = app.listen(process.env.PORT || 3000);
 
 module.exports = server;
+
